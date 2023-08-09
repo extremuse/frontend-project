1e5ad36598e6fa35151642dc3a798f157b3e37f8
sachin kumar <sachinkumar@sachins-MacBook-Air.local>
2023-08-09 10:28:17 +0530

    first commit

--- script.js
+++ script.js
@@ -1,52 +1,45 @@
 window.onload = function() {
-    // This event listener waits for the page to fully load before executing its code.
-    // This ensures that all elements are present and ready to be manipulated.
 
     const cards = document.querySelectorAll('.card');
-    // This selects all elements with the class "card" and stores them in the "cards" variable.
-
+    const totalPrize = document.getElementById('total-prize')
+    let selectedProductPrize = 0;
     cards.forEach(card => {
-        // This loops through each card element that was selected earlier.
-
         const dropdown = card.querySelector('.dropdown');
-        // This selects the element with class "dropdown" inside the current card and stores it in "dropdown".
-
         const radio = card.querySelector('input[type="radio"]');
-        // This selects the radio button input inside the current card and stores it in "radio".
-
+        
         card.addEventListener('click', function(event) {
-            // This adds a click event listener to the current card element.
-
             if (dropdown.classList.contains('show-dropdown')) {
-                // This checks if the dropdown already has the class "show-dropdown".
-
                 dropdown.classList.remove('show-dropdown');
-                // If the dropdown has the class, it is removed to hide the dropdown.
-
                 radio.checked = false;
-                // The associated radio button is unchecked.
-
+                selectedProductPrize = 0; // Reset the selected product's prize
             } else {
                 dropdown.classList.add('show-dropdown');
-                // If the dropdown doesn't have the class, it is added to show the dropdown.
-                
                 radio.checked = true;
-                // The associated radio button is checked.
+                
+                // Update the selected product's prize based on the clicked product
+                if (radio.value === 'onePair') {
+                    selectedProductPrize =  195.00;
+                } else if (radio.value === 'secondPair') {
+                    selectedProductPrize =345.00;
+                }
+                 else if (radio.value === 'ThirdPair') {
+                    selectedProductPrize =  528.00;
+                }
             }
+            
+            // Update the total prize display
+            totalPrize.textContent = `Total Prize: ${selectedProductPrize.toFixed(2)}`;
         });
-
+        
         dropdown.addEventListener('click', function(event) {
             event.stopPropagation();
-            // This prevents the click event on the dropdown from reaching the parent card.
-            // This prevents the dropdown from closing when clicked inside.
         });
-
+        
         radio.addEventListener('change', function() {
             if (!radio.checked) {
-                // This checks if the radio button is not checked.
-
                 dropdown.classList.remove('show-dropdown');
-                // If the radio button is unchecked, the dropdown is hidden.
+                selectedProductPrize = 0; // Reset the selected product's prize
+                totalPrize.textContent = `Total Prize: ${selectedProductPrize.toFixed(3)}`;
             }
         });
     });
